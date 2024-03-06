7c9ef2572bcfbce6d4676f8826a38e4c2aeeaee7
IRSHAD <53285011+irshad24@users.noreply.github.com>
2024-03-06 10:48:43 +0400

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -163,4 +163,4 @@ def csv():
         return send_file(o_file,as_attachment=True)
     return  render_template("index.html")
 if __name__=="__main__":
-     app.run(host='127.0.0.9',port=4455,debug=True) 
+     app.run(host='127.0.0.9',port=4455) 
