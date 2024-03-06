50503c7a5263228672fb4a7fbf6626e42fd517fe
IRSHAD <53285011+irshad24@users.noreply.github.com>
2024-03-06 10:07:28 +0400

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -164,6 +164,6 @@ def csv():
     return  render_template("index.html")
 if __name__=="__main__":
     from waitress import serve
-    serve(app, host="1.1.1.1", port=8880)
+    serve(app, host="0.0.0.0", port=6677)
     app.debug=True
     app.run()
